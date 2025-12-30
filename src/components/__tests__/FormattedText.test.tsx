1ed0209d12c10cb16c7b4a58799d6c38a067091c
Wade <wadezyw@gmail.com>
2025-12-30 22:41:46 +0800

    Update npm dependency (#15)

--- src/components/__tests__/FormattedText.test.tsx
+++ src/components/__tests__/FormattedText.test.tsx
@@ -1,5 +1,6 @@
 import React from "react";
-import { render, screen } from "@testing-library/react";
+import { render } from "@testing-library/react";
+import { screen } from "@testing-library/dom";
 import { FormattedText } from "../FormattedText";
 import { TranslationContext } from "../../context/TranslationContext";
 
